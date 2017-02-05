16eabed4d20e62ba930e81e8be464472b732b8cb
“JASON” <jrcorey84@gmail.com>
2017-02-04 20:18:47 -0600

    finished layout 3 and js problem 2

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+// QUESTION 1
+
+for(var i = 20; i <= 50; i ++){
+	if(i % 3 === 0)
+		console.log(i)
+	else{
+		console.log("")
+	}
+}
+
+// QUESTION 2
+
+for(var i = 0; i >= 0; i ++){
+	console.log(i)
+}
+
+// QUESTION 3
