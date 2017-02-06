a17e510600474d48ce7eee251ddb119e9a738fa1
“JASON” <jrcorey84@gmail.com>
2017-02-05 20:52:11 -0600

    finished js problems

--- js/script.js
+++ js/script.js
@@ -28,3 +28,21 @@ for(var i=0; i<= classmates.length; i++){
 
 // Question 4
 
+var menu = [{name: "pizza", glutenFree: false},{name: "salad", glutenFree: true},{name: "donut", glutenFree: false},{name: "steak", glutenFree: true},{name: "chicken", glutenFree: true},{name: "cheeseburger", glutenFree: false}];
+var safe = []
+
+for(var i=0; i<menu.length; i++){
+	if(menu[i].glutenFree === true)
+		safe.push(menu[i].name)
+}
+
+// Bonus!!!!
+
+var league = [{name: "NFL", players: [{name: "Tom Brady", goat: false}, {name: "Walter Payton", goat: true}]}, {name: "NBA", players: [{name: "Michael Jordan", goat: true}, {name: "Lebron James", goat: false}]},{name: "MLB", players: [{name: "Babe Ruth", goat: true}, {name: "Alex Rodriguez", goat: false}]}]
+var goatPlayers = []
+
+for(i=0; i<league.length; i++){
+	if(league[i].players[i].goat === true){
+		goatPlayers.push(league[i].players[i].name)
+	}
+}
